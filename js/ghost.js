6d83823632dbb0f90a67b3c7a38425028d0d3858
Unknown <clemclem011@gmail.com>
2018-11-21 09:04:28 +0100

    Add ghost

---
+++ js/ghost.js
@@ -0,0 +1,17 @@
+function Ghost(width, height, posX, posY, color)
+{
+	
+	this.width = width;
+	this.height = height;
+	this.posX = posX;
+	this.posY = posY;
+	this.color = color;
+
+	this.create() = function(path)
+	{
+		
+	},
+
+
+
+}
